04c012568db884eb4c1a160aa9c003bb02993dc9
Nicholas Willard <nswillard@hotmail.com>
2015-09-05 15:46:35 -0500

    Commit the changes to GPSListener class. Forgot to git add

--- app/src/main/java/edu/rasmussen/mobile/project06/GPSListener.java
+++ app/src/main/java/edu/rasmussen/mobile/project06/GPSListener.java
@@ -1,33 +1,45 @@
 package edu.rasmussen.mobile.project06;
 
+import android.content.Context;
 import android.location.LocationListener;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Toast;
 
 /**
  * Created by nicho_000 on 9/5/2015.
  */
-public class GPSListener extends LocationListener {
+
+public class GPSListener implements LocationListener {
     public static double latitude;
     public static double longitude;
+    Context tContext = getApplicationContext();
+    String tDisabledText = "GPS is Disabled";
+    String tEnabledText = "GPS is Enabled";
+    int tDuration = Toast.LENGTH_SHORT;
 
-    @Override
-    public void onLocationChange (Location loc) {
-        loc.getLatitude();
-        loc.getLongitude();
-        latitude=loc.getLatitude();
-        longitude=loc.getLongitude();
-    }
 
     @Override
     public void onProviderDisabled(String provider) {
-        Toast.makeText(getBaseContext(), "GPS is disabled.", Toast.LENGTH_SHORT).show();
+        Toast tToast = Toast.makeText(tContext, tDisabledText, tDuration);
+        tToast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL, 0, 0);
+        tToast.show();
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-        Toast.makeText(getBaseContext(), "GPS is enabled.", Toast.LENGTH_SHORT).show();
+        Toast tToast = Toast.makeText(tContext, tEnabledText, tDuration);
+        tToast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL, 0, 0);
+        tToast.show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        location.getLatitude();
+        location.getLongitude();
+        latitude=location.getLatitude();
+        longitude=location.getLongitude();
     }
 
     @Override
